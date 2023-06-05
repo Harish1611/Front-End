687ed5f5397e52a35108245c9f39cfff87a610e6
Harish1611 <harishnaidub005@gmail.com>
2023-06-05 15:43:54 +0530

    version 2.0

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 console.log("This is Demo App JS");
+console.log("Version 2.0 Data has been added");
--- demo.js
+++ demo.js
@@ -1,1 +1,2 @@
 console.log("This is second demo app JS");
+console.log("Version 2.0 Data has been added");
